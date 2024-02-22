0d6c8985a36f134584eae887fa41b9a6644712cd
Damian <a.damianrodriguez.93@gmail.com>
2024-02-22 12:00:13 -0300

    -

--- src/components/administrador/screen-bar-admin.tsx
+++ src/components/administrador/screen-bar-admin.tsx
@@ -7,13 +7,11 @@ export const ScreenBarAdmin = ({ onOptionSelect }) => {
 
   return (
     <>
-
-
       <div className="userOptions">
         <div className="logoContainer">
           <img className="logo" src="https://infracciones-app.s3.sa-east-1.amazonaws.com/la_rioja_logo.jpeg" alt="Logo" />
         </div>
-        <button onClick={() => handleOptionClick('Gestion de usuarios')}>Gestion de usuarios</button>
+        <button onClick={() => handleOptionClick('Gestion de usuarios')}>Gestión de usuarios</button>
         <button onClick={() => handleOptionClick('Trasnportes')}>Transportes</button>
         <button onClick={() => handleOptionClick('Taxis-remises')}>Taxis y Remises</button>
         <button onClick={() => handleOptionClick('turismo')}>Turismo</button>
